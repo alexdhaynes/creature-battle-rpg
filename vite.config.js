6c4cfd7b2081c992356c4013e9590806f0e3ace3
Alex Haynes <85761754+alexdhaynes@users.noreply.github.com>
2024-09-10 10:57:49 -0400

    add postcss for easier css styling

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@scripts': path.resolve(__dirname, './src/scripts'),
+      '@game': path.resolve(__dirname, './src/scripts/game'),
       '@styles': path.resolve(__dirname, './src/styles'),
     },
   },
